6260067948d07c161cd01094084503bf794c3ef5
raypark1241 <raypark1241@gmail.com>
2023-12-19 10:12:15 +0900

    views.py search class

--- backends/eightfortypy/views.py
+++ backends/eightfortypy/views.py
@@ -6,6 +6,7 @@ import datetime
 import base64
 from eightfortypy.models import * 
 import pymysql
+import requests
 
 # forms.py 
 from eightfortypy.forms import * 
@@ -71,66 +72,37 @@ class ProfileUpdateView(UpdateView):
 # cursor.close()
 # connection.close()
 
-# class UseApi:
-#     def __init__(self, cli_id, cli_sec):
-#         client_id = cli_id
-#         client_secret = cli_sec
-#         token_endpoint = 'https://accounts.spotify.com/api/token'
-#         payload = {'grant_type': 'client_credentials'}
-#         headers = {
-#         'Content-Type': 'application/x-www-form-urlencoded',
-#         'Authorization': 'Basic ' + base64.b64encode(f"{client_id}:{client_secret}".encode()).decode()
-#         }
-#         token_response = requests.post(token_endpoint, data=payload, headers=headers)
-#         token_data = token_response.json()
-#         access_token = token_data['access_token']
-#         # Fetch data from Spotify API using search query
-#         self.headers = {'Authorization': 'Bearer ' + access_token}
-#         print("Authorization complete")
+class UseApi:
+    def __init__(self, cli_id, cli_sec):
+        client_id = cli_id
+        client_secret = cli_sec
+        token_endpoint = 'https://accounts.spotify.com/api/token'
+        payload = {'grant_type': 'client_credentials'}
+        headers = {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Authorization': 'Basic ' + base64.b64encode(f"{client_id}:{client_secret}".encode()).decode()
+        }
+        token_response = requests.post(token_endpoint, data=payload, headers=headers)
+        token_data = token_response.json()
+        access_token = token_data['access_token']
+        # Fetch data from Spotify API using search query
+        self.headers = {'Authorization': 'Bearer ' + access_token}
+        print("Authorization complete")
         
-#     def search_track(self, track_name):
-#         search_endpoint = f'https://api.spotify.com/v1/search?q={track_name}&type=track'
-#         search_response = requests.get(search_endpoint, headers=self.headers)
-#         search_data = search_response.json()
-#         track_id = search_data['tracks']['items'][0]['id']  # Get track ID from the first search result
-#         # Fetch track data from Spotify API using track ID
-#         track_endpoint = f'https://api.spotify.com/v1/tracks/{track_id}'
-#         track_response = requests.get(track_endpoint, headers=self.headers)
-#         self.track_data = track_response.json()
-#         return self.track_data
-    
-#     def to_database(self, mysql_connection_info):
-#         connection = pymysql.connect(**mysql_connection_info)
-#         cursor = connection.cursor()
-#         if not self.track_data:
-#             return
-
-#         track_info = self.track_data
-
-#         # Music 모델에 데이터 삽입
-#         try:
-#             Song.objects.create(
-#                 album=track_info['album']['name'],
-#                 title=track_info['name'],
-#                 popularity = track_info["popularity"],
-#                 id = track_info["id"],
-#                 link = track_info["external_urls"]["spotify"]
-#             )
-#             Album.objects.create(
-#                 title=track_info["album"]["name"],
-#                 artist = track_info["album"]["artists"][0]["name"],
-#                 release_Date=datetime.strptime(track_info['album']['release_date'], '%Y-%m'),  # 날짜 형식 변환 필요
-#                 album_image=track_info["album"]["images"][0]["url"],
-#             )
-#             Artist.objects.create(
-#                 name = track_info["album"]["artists"][0]["name"],
-#                 image = track_info,
-#                 id = track_info["artists"][0]["id"],
-#                 genres=', '.join(track_info['artists'][0]['genres']),  # 리스트 형태의 장르를 문자열로 변환
-#             )
-#         except Exception as e:
-#             print(f"Failed to insert data: {e}")
-#         cursor.close()
-#         connection.close()
-    
-    
+    def search_track(self, track_name):
+        search_endpoint = f'https://api.spotify.com/v1/search?q={track_name}&type=track'
+        search_response = requests.get(search_endpoint, headers=self.headers)
+        search_data = search_response.json()
+        track_id = search_data['tracks']['items'][0]['id']  # Get track ID from the first search result
+        
+        track_endpoint = f'https://api.spotify.com/v1/tracks/{track_id}'
+        track_response = requests.get(track_endpoint, headers=self.headers)
+        self.track_data = track_response.json()
+        artist_name = self.track_data['artists'][0]['name']
+        artist_data = self.search_artist(artist_name)
+        
+        self.track_data['artist_info'] = artist_data  # Adding artist info to track_data
+        self.album_data = dict(album_id = self.track_data["album"]["id"], album_name = self.track_data["album"]["name"], release_data = self.track_data["album"]["release_date"], album_image = self.track_data["album"]["images"][0]["url"], album_link = self.track_data["album"]["external_urls"])
+        self.artist_data = dict(artist_id = self.track_data["artist_info"]["id"], artist_name = self.track_data["artist_info"]["name"], artist_popularity = self.track_data["artist_info"]["popularity"], artist_genres = ",".join(self.track_data["artist_info"]), artist_image = self.track_data["artist_info"]["images"][0]["url"], artist_link = self.track_data["artist_info"]["external_urls"])
+        self.song_data = dict(track_id = self.track_data["id"], track_name = self.track_data["name"], track_popularity = self.track_data["popularity"], track_image = self.track_data["album"]["images"][0]["url"], track_link= self.track_data["external_urls"])
+        return self.album_data, self.artist_data, self.song_data
